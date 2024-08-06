bf9508305820223363cf554b11391d0c0d1cfb31
VedikaMamidwar <vedikamamidwar5@gmail.com>
2024-08-06 18:05:39 +0530

    done changes

--- index1.js
+++ index1.js
@@ -1,9 +1,5 @@
-const numbers = [100,116,213,14,104]
-let sum =0;
-for(let i=0; i<numbers.length; i++)
-{
-    if(numbers[i]%2==1){
-        sum+= numbers[i]
-    }
+let myArray = ['Alice', 'Bob', 'Charlie', 'Diana'];
+
+for (let element of myArray) {
+  console.log('Hello ' + element);
 }
-console.log(`sum${sum}`)
